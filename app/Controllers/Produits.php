b9d8a7aa42aaea917348ff844d48a6217bf636ab
Florian4834 <florianfernandes342001@gmail.com>
2026-03-04 15:03:16 +0100

    Suppression de la méthode index

--- app/Controllers/Produits.php
+++ app/Controllers/Produits.php
@@ -7,19 +7,6 @@ use App\Models\Produit;
 
 class Produits extends BaseController
 {
-    public function getIndex()
-    {
-        $data = [
-            "activePage" => 'home',
-            "categories" => Categorie::all(),
-            "page" => 'Accueil'
-        ];
-
-        return view('template/head', $data)
-            . view('template/header', $data)
-            . view('index.php', $data)
-            . view('template/footer', $data);
-    }
 
     public function getProduitsliste($categorie) {
         $produits = null;
