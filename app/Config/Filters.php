463c69e3d518f796dfde90f589c122bb87de348f
Florian4834 <florianfernandes342001@gmail.com>
2026-03-04 14:57:30 +0100

    Ajout des filtres

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -2,6 +2,8 @@
 
 namespace Config;
 
+use App\Filters\AdminAuthFilter;
+use App\Filters\CustomerAuthFilter;
 use CodeIgniter\Config\Filters as BaseFilters;
 use CodeIgniter\Filters\Cors;
 use CodeIgniter\Filters\CSRF;
@@ -34,6 +36,8 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'adminauth'     => AdminAuthFilter::class,
+        'customerauth'  => CustomerAuthFilter::class,
     ];
 
     /**
@@ -106,5 +110,23 @@ class Filters extends BaseFilters
      *
      * @var array<string, array<string, list<string>>>
      */
-    public array $filters = [];
+    public array $filters = [
+        'adminauth' => [
+            'before' => [
+                'admin',
+                'prodgest/*',
+                'categgest/*',
+                'commgest/*',
+                'comptgest/*',
+                'ajax/*'
+            ]
+        ],
+        'customerauth' => [
+            'before' => [
+                'account',
+                'account/*',
+                'auth/deleteaccountdata'
+            ]
+        ]
+    ];
 }
